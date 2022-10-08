e2d5d44abe4e43d339cc681fed6bfddee9330afc
CreateSource <72283721+abnormalhare@users.noreply.github.com>
2022-10-07 22:49:01 -0400

    testing

--- MiniChipOS/index.js
+++ MiniChipOS/index.js
@@ -1,24 +1,8 @@
-function hashCode(str) { // java String#hashCode
-    var hash = 0;
-    for (var i = 0; i < str.length; i++) {
-       hash = str.charCodeAt(i) + ((hash << 5) - hash);
-    }
-    return hash;
-} 
-
-function intToRGB(i){
-    var c = (i & 0x00FFFFFF)
-        .toString(16)
-        .toUpperCase();
-
-    return "00000".substring(0, 6 - c.length) + c;
-}
-
 const check_text = (text) => {
     let split = text.innerHTML.split(" ")
+    console.log(split, split[0])
     switch (split[0]) {
         case "color":
-            console.log("this works")
             text.style = "background-color: " + split[1]
             break
         case "quit":
