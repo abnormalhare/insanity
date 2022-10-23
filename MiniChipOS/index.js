6a58df83d6fa29e6903035b6eca989b3028e4428
CreateSource <72283721+abnormalhare@users.noreply.github.com>
2022-10-23 19:22:13 -0400

    lol

--- MiniChipOS/index.js
+++ MiniChipOS/index.js
@@ -1,28 +1,10 @@
 const determineColor = (value) => {
-    let out = value.replace("#", "")
-    try {
-        out2 = Number(out)
-        console.log(out2)
-        if (out.length < 4 && out2 < 555) {
-            document.body.style.color = "white"
-            console.log("White: < 4, < 555")
-        } else if (out.length > 4 && out2 < 555555) {
-            document.body.style.color = "white"
-            console.log("White: > 4, < 555555")
-        } else {
-            document.body.style.color = "black"
-            console.log("black")
-        }
-        out = "#" + out2
-        console.log(out)
-    } catch (e) {
-        if (out == "black" || out.indexOf("dark") != -1) {
-            document.body.style.color = "white"
-        } else {
-            document.body.style.color = "black"
-        }
+    if (value == "black" || value.indexOf("dark") != -1) {
+        document.body.style.color = "white"
+    } else {
+        document.body.style.color = "black"
     }
-    document.body.style.backgroundColor = out
+    document.body.style.backgroundColor = value
 }
 
 const check_text = (text) => {
