3eb19570a0492262c1f6ff9d26f9c53dfeb607b5
CreateSource <72283721+abnormalhare@users.noreply.github.com>
2022-12-18 16:30:38 -0500

    almost working...

--- EoDMM/index.js
+++ EoDMM/index.js
@@ -1,5 +1,5 @@
 const sectionList = [
-    {type: "chapter", chapter: "1.2.6", end: 634},
+    {type: "chapter", chapter: "1.2.6", end: 633},
     {type: "an", end: 651},
     {type: "anabs", end: 665},
     {type: "anand", end: 669},
@@ -17,10 +17,10 @@ const typeNext = document.getElementById("TN")
 const areaNext = document.getElementById("AN")
 
 let chapter = section;
-while (sectionList[chapter].chapter != null) {
+while (sectionList[chapter].chapter != undefined) {
     chapter -= 1
 }
 
 chapCurr.innerHTML = sectionList[chapter].chapter
 typeCurr.innerHTML = ""+sectionList[section].type
-areaCurr.innerHTML = sectionList[section - 1].end + "-" + sectionList[section].end
+areaCurr.innerHTML = (sectionList[section - 1].end + 1) + "-" + sectionList[section].end
