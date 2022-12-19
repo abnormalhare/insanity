8b67a64b557b961e65a2432885c0573e04244886
CreateSource <72283721+abnormalhare@users.noreply.github.com>
2022-12-19 09:43:39 -0500

    Soy estupido

--- EoDMM/spcSect.js
+++ EoDMM/spcSect.js
@@ -2,8 +2,8 @@ const amountSPC = {
     start: 1,
     chapters: [2, 2, 4, 104, 84, 187, 490]
 }
-let locationSPC = 670
 const setPercentage = (block) => {
+    let locationSPC = 670
     let indexSPC = 0
     let i = amountSPC.chapters[indexSPC]
     while (indexSPC < parseInt(block.id)) {
