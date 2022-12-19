be8542956c390e34627fc60efc38e1f933114120
CreateSource <72283721+abnormalhare@users.noreply.github.com>
2022-12-19 09:08:50 -0500

    conflict resolution?

--- EoDMM/spcSect.js
+++ EoDMM/spcSect.js
@@ -1,13 +1,13 @@
-const amount = {
+const amountSPC = {
     start: 1,
     chapters: [2, 2, 4, 104, 84, 187, 490]
 }
-let location = 2
-let index = 0
-let i = amount.chapters[index]
-while (location - i > 0) {
-    location -= i;
-    index++;
-    i = amount.chapters[index];
+let locationSPC = 2
+let indexSPC = 0
+let i = amountSPC.chapters[indexSPC]
+while (locationSPC - i > 0) {
+    locationSPC -= i;
+    indexSPC++;
+    i = amountSPC.chapters[indexSPC];
 }
-console.log(location)
+console.log(locationSPC)
