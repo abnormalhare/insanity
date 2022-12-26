e23f4f2f606f4c9a7ef539f49dbafe81f84d4991
CreateSource <72283721+abnormalhare@users.noreply.github.com>
2022-12-26 14:47:06 -0500

    Update mainSect.js

--- EoDMM/mainSect.js
+++ EoDMM/mainSect.js
@@ -29,7 +29,7 @@ const sectionList = [
     {type: "jao", end: 986}
 ]
 ///// USE FOR UPDATING TO NEXT SECTION /////
-let section = 22
+let section = 23
 /////         VERY IMPORTANT           /////
 const chapCurr = document.getElementById("CC")
 const typeCurr = document.getElementById("TC")
