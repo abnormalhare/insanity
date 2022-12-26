a160d2205be53ea7799fd3a04537c4fcc0129f84
CreateSource <72283721+abnormalhare@users.noreply.github.com>
2022-12-25 22:09:38 -0500

    Update spcSect.js

--- EoDMM/spcSect.js
+++ EoDMM/spcSect.js
@@ -4,7 +4,7 @@ const amountSPC = {
 let locationG;
 const setPercentage = (block) => {
     ///// important
-    let locationSPC = 897
+    let locationSPC = 901
     /////
     locationG = locationSPC
     let indexSPC = 0
