66ac6ba983577282f82710a9b47197dce572a151
CreateSource <72283721+abnormalhare@users.noreply.github.com>
2022-12-27 18:37:44 -0500

    Update jacSect.js

--- EoDMM/jacSect.js
+++ EoDMM/jacSect.js
@@ -4,7 +4,6 @@ const amountSPC = {
 ///// important
 let locationG = 1014;
 /////
-let hasPoint = false;
 const setPercentage = (block) => {
     let locationSPC = locationG;
     let amount = 1;
@@ -25,7 +24,6 @@ const setPercentage = (block) => {
         block.innerHTML = "100%";
         if (percentage == 100)
             block.innerHTML = "100%!";
-            hasPoint = true;
     } else {
         if (percentage >= 75) {
             block.className = "ygBefore";
@@ -45,6 +43,6 @@ const setPercentage = (block) => {
 }
 const position = () => {
     let posOut = locationG
-    if (!(locationG % 100) || hasPoint) posOut += "!"
+    if (!(locationG % 100)) posOut += "!"
     document.getElementById("LUP").innerHTML = posOut
 }
