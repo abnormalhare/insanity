a436300a69fcd491a492ef8eaacb0c9060bbe9f2
CreateSource <72283721+abnormalhare@users.noreply.github.com>
2023-07-16 22:48:57 -0400

    rewrite

--- metamath/script.js
+++ metamath/script.js
@@ -1,19 +1,57 @@
-let meta = 0;
+// Set initial amount of meta
+let metaAmount = 3;
 
-function updateGame() {
-    document.getElementById("meta-amount").textContent = meta;
+// Update meta count on the page
+function updateMetaCount() {
+  const metaCountElement = document.getElementById("meta-amount");
+  metaCountElement.textContent = metaAmount;
 }
 
-function purchaseItem() {
-    const cost = 10;
-    if (meta >= cost) {
-        // Deduct the cost from meta
-        meta -= cost;
-
-        // Handle the item purchase logic
-        // ...
-
-        // Update the game display
-        updateGame();
-    }
+// Increase meta count when an item is purchased
+function purchaseItem(cost) {
+  if (metaAmount >= cost) {
+    metaAmount -= cost;
+    updateMetaCount();
+    alert("Item purchased successfully!");
+  } else {
+    alert("Not enough meta to purchase this item!");
+  }
 }
+
+// Event listener for purchasing the Phi item
+const phiItem = document.getElementById("phi-item");
+phiItem.addEventListener("click", function () {
+  purchaseItem(2);
+});
+
+// Event listener for purchasing the Psi item
+const psiItem = document.getElementById("psi-item");
+psiItem.addEventListener("click", function () {
+  purchaseItem(3);
+});
+
+// Event listener for purchasing the Chi item
+const chiItem = document.getElementById("chi-item");
+chiItem.addEventListener("click", function () {
+  purchaseItem(4);
+});
+
+// Event listener for purchasing the Not item
+const notItem = document.getElementById("not-item");
+notItem.addEventListener("click", function () {
+  purchaseItem(1);
+});
+
+// Event listener for purchasing the Imply item
+const implyItem = document.getElementById("imply-item");
+implyItem.addEventListener("click", function () {
+  purchaseItem(2);
+});
+
+// Initialize the game
+function initGame() {
+  updateMetaCount();
+}
+
+// Start the game
+initGame();
