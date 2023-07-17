2c1b9532af7495acc2a1a71f9d12a55082372b38
CreateSource <72283721+abnormalhare@users.noreply.github.com>
2023-07-16 21:35:50 -0400

    a

--- metamath/script.js
+++ metamath/script.js
@@ -2,21 +2,18 @@ let meta = 0;
 
 function updateGame() {
     document.getElementById("meta-amount").textContent = meta;
-    // Generate and update purchasable items dynamically
-
-    // Example item:
-    const itemHTML = `
-        <div class="item">
-            <p>Item Name</p>
-            <p>Cost: 10 Meta</p>
-            <button onclick="purchaseItem()">Purchase</button>
-        </div>
-    `;
-    document.getElementById("resources").innerHTML += itemHTML;
 }
 
 function purchaseItem() {
-    // Handle item purchase logic
-}
+    const cost = 10;
+    if (meta >= cost) {
+        // Deduct the cost from meta
+        meta -= cost;
 
-updateGame();
+        // Handle the item purchase logic
+        // ...
+
+        // Update the game display
+        updateGame();
+    }
+}
