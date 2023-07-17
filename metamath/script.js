f3b9efaf341385f966697712948a2a64acefc36e
CreateSource <72283721+abnormalhare@users.noreply.github.com>
2023-07-16 22:34:02 -0400

    asfasfgas

--- metamath/script.js
+++ metamath/script.js
@@ -17,3 +17,15 @@ function purchaseItem() {
         updateGame();
     }
 }
+// Calculate and set the width of the meta container dynamically
+function updateMetaContainerWidth() {
+    const itemContainerWidth = document.querySelector(".item-container").offsetWidth;
+    const itemCount = document.querySelectorAll("#variable-syntax-container .item-container").length;
+    const marginTotal = (itemCount - 1) * 2; // Assuming 2px margin on both sides of each item container
+    const metaContainerWidth = itemContainerWidth * itemCount + marginTotal;
+    document.getElementById("meta-container").style.width = metaContainerWidth + "px";
+}
+
+// Call the function initially and whenever the window is resized
+updateMetaContainerWidth();
+window.addEventListener("resize", updateMetaContainerWidth);
