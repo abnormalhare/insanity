87c764ee9d4ae316a603a2d69c3afc6bfbc46b89
CreateSource <72283721+abnormalhare@users.noreply.github.com>
2023-07-23 00:37:40 -0400

    Update jacSect.js

--- EoDMM/jacSect.js
+++ EoDMM/jacSect.js
@@ -2,7 +2,7 @@ const amountSPC = {
     chapters: [2, 2, 4, 104, 85, 188, 448, 104, 106, 18, 6, 396, 25]
 }
 ///// important
-let locationG = 1049;
+let locationG = 1050;
 /////
 const setPercentage = (block) => {
     let locationSPC = locationG;
