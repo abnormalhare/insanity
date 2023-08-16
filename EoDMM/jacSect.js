df47fc62ffccb6901cb245bb24655814122a7914
CreateSource <72283721+abnormalhare@users.noreply.github.com>
2023-08-16 17:28:53 -0400

    Update jacSect.js

--- EoDMM/jacSect.js
+++ EoDMM/jacSect.js
@@ -1,6 +1,6 @@
 ///// important
 const chapters = [2, 2, 4, 104, 85, 188, 448, 104, 106, 17, 6, 396, 25, 17, 26, 26]
-let locationG = 1390;
+let locationG = 1400;
 let sectPercentageList = [];
 let sectNumStart = 0;
 /////
