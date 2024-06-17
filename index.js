82878384214646800953d0e54e0f267f1285b006
CreateSource <72283721+abnormalhare@users.noreply.github.com>
2024-06-16 23:19:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+function redirectTo(url) {
+    window.location.href = url;
+}
+
 function detectMinecraftClient() {
     var userAgent = navigator.userAgent;
     if (userAgent.includes("Minecraft")) {
         console.log("Minecraft client detected");
     } else {
-        console.log("Not a Minecraft client");
+        redirectTo("98.180.36.83:12345")
     }
 }
 
