efee06781cce35781ffcb3b80f529613960b6f89
CreateSource <72283721+abnormalhare@users.noreply.github.com>
2024-06-23 08:37:44 -0400

    alert people of the news

---
+++ index.js
@@ -0,0 +1,11 @@
+function detectChromeBrowser() {
+    if (navigator.userAgent.indexOf("Chrome") != 1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+if (detectChromeBrowser()) {
+    alert("Google is removing the ability to use ad-blockers on all chromium browsers!\nPlease use Firefox with uBlock Origin or Brave with shields!")
+}
